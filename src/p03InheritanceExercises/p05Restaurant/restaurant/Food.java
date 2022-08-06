7317ffcb1c5f466ef5a8ff92268314c42d4b7754
Dimitar Rachev <DesolateBG@gmail.com>
2022-08-07 00:42:01 +0300

    vehicle

---
+++ src/p03InheritanceExercises/p05Restaurant/restaurant/Food.java
@@ -0,0 +1,16 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Food extends Product{
+    protected double grams;
+
+    public Food(String name, BigDecimal price, double grams) {
+        super(name, price);
+        this.grams = grams;
+    }
+
+    public double getGrams() {
+        return grams;
+    }
+}
